4fb151fc77df166bb8db75941cb60d48aded6ffa
youhanna <youhanna.production@gmail.com>
2025-04-10 19:55:16 -0400

    Added cell scaling to fill screen at all times
    - Added extra elements to level preset
    - Updated button Animation
    - Added code base scaling for cells

--- Assets/Scripts/UI/CanvasManager.cs
+++ Assets/Scripts/UI/CanvasManager.cs
@@ -12,6 +12,8 @@ public class CanvasManager : MonoBehaviour
     [SerializeField]
     private Card _cardPrefab;
     [SerializeField]
+    private RectTransform _gridContainer;
+    [SerializeField]
     private GridLayoutGroup _gridLayoutGroup;
     private List<Card> _cards = new List<Card>();
     [Header("HUD")]
@@ -22,6 +24,12 @@ public class CanvasManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _streak;
     public Action ResetLevelsCallback;
+    private float _maxColumns = 7;
+
+    private void Start()
+    {
+        _maxColumns = _gridLayoutGroup.constraintCount;
+    }
 
     /// <summary>
     /// Use template to generate a new card set
@@ -31,7 +39,6 @@ public class CanvasManager : MonoBehaviour
     public void GenerateCards(List<CardData> set, int columnCount, float revealTime, CardEventHandler selectionCallback = null)
     {
         ClearCards();
-        _gridLayoutGroup.constraintCount = columnCount; 
 
         for (int i = 0; i < set.Count; i++)
         {
@@ -40,6 +47,13 @@ public class CanvasManager : MonoBehaviour
             card.OnCardSelected += selectionCallback;
             _cards.Add(card);
         }
+
+        //Calculating the size of the grid cell
+        _gridLayoutGroup.constraintCount = columnCount;
+        float rowCount = Mathf.Ceil(set.Count / (float)columnCount);
+        float scaleFactorX = (_gridContainer.sizeDelta.x - (columnCount * _gridLayoutGroup.spacing.x + _gridLayoutGroup.padding.left + _gridLayoutGroup.padding.right)) / columnCount;
+        float scaleFactorY = (_gridContainer.sizeDelta.y - (rowCount * _gridLayoutGroup.spacing.y + _gridLayoutGroup.padding.top + _gridLayoutGroup.padding.bottom)) / rowCount;
+        _gridLayoutGroup.cellSize = new Vector2(scaleFactorX, scaleFactorY);
     }
 
     public void ClearCards()
