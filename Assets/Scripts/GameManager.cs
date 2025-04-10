b7e7b7874e8942ce43d1b61e19fcf7873e11f290
youhanna <youhanna.production@gmail.com>
2025-04-10 17:18:00 -0400

    Added basic audio system
    - Added audio manager with pseudo-pooling
    - Added Audio lookup table
    - Added clips for match, mismatch and winning a level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,7 @@
+using Audio;
 using Progression;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using Tools;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,6 +11,8 @@ public class GameManager : MonoBehaviour
     private CanvasManager _canvasManager;
     [SerializeField]
     private LevelPresets _presets;
+    [SerializeField]
+    private AudioManager _audioManager;
 
     //progression handlings
     private ProgressionHandler _progressionHandler = new ProgressionHandler();
@@ -92,6 +93,7 @@ public class GameManager : MonoBehaviour
                 previous.Close();
                 //reset streak
                 _streak = 0;
+                _audioManager.PlayAudioById(AudioManager.MISMATCH_ID);
                 OnStreakUpdated?.Invoke(_streak);
             }
         }
@@ -106,6 +108,7 @@ public class GameManager : MonoBehaviour
         _score += value;
         _streak++;
         progression--;
+        _audioManager.PlayAudioById(AudioManager.MATCH_ID);
 
         OnScoreUpdated?.Invoke(_score);
         OnStreakUpdated?.Invoke(_streak);
@@ -126,6 +129,7 @@ public class GameManager : MonoBehaviour
 
     public void WinLevel()
     {
+        _audioManager.PlayAudioById(AudioManager.LEVEL_UP_ID, true);
         CreateLevel();
         _progressionHandler.UpdateProgress(_level - 1, _score, _streak);
         _progressionHandler.SaveProgress();
